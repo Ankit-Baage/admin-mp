db4ec6bc99b4e8d5f7425b977e180dcfaaa4230b
Ankit Baage <ankitbaage3@gmail.com>
2024-09-01 21:24:32 +0530

    advertisement-file-upload

--- src/component/fileUploadInput/FileUploadInput.js
+++ src/component/fileUploadInput/FileUploadInput.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import classes from "./fileUploadInput.module.css";
 import { uploadImageRequest } from "../../http-request/uploadImageRequest";
 
+import uploadIcon from "../../assets/upload-icon.svg";
+import classes from "./fileUploadInput.module.css";
 
 export const FileUploadInput = ({ urlWithExt, url, onSelection }) => {
   const [uploadImage, setUploadImage] = useState("");
@@ -19,7 +20,7 @@ export const FileUploadInput = ({ urlWithExt, url, onSelection }) => {
       const imageUrl = response.data.fileUrl;
       setUploadImage(imageUrl);
       console.log(imageUrl);
-      onSelection(imageUrl)
+      onSelection(imageUrl);
     } catch (error) {
       console.error(`Upload failed for :`, error);
     }
@@ -27,9 +28,11 @@ export const FileUploadInput = ({ urlWithExt, url, onSelection }) => {
 
   return (
     <div className={classes.form__group}>
-      <h3 className={classes.form__field__upload__title}>"Upload File</h3>
+      <h3 className={classes.form__field__upload__title}>Upload File</h3>
       <div className={classes.form__group__upload}>
         <label htmlFor="url" className={classes.form__field__upload__label}>
+          <img src={uploadIcon} alt="upload" className={classes.upload__img} />
+          <h3 className={classes.upload__label}>Upload File</h3>
           <input
             name="url"
             type="file"
