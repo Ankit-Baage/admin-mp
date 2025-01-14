1d0c48528d606440610ee1d62403230fd690f069
Ankit Baage <ankitbaage3@gmail.com>
2025-01-14 14:06:51 +0530

    cosmetic -c-sidebar

--- src/component/sideBar/dropDown.js
+++ src/component/sideBar/dropDown.js
@@ -11,18 +11,18 @@ import advertisement from "../../assets/advertisement__sidebar.svg"
 export const dropdowns = [
   {
     id: "category",
-    title: "Categories",
+    title: "Manage Inventory",
     options: [
       { id: "vrp", image: vrp, name: "VRP", path: "vrp" },
-      { id: "spares", image: spare, name: "SPARES", path: "spares" },
+      { id: "spares", image: spare, name: "Spares", path: "spares" },
       {
         id: "new_phones",
         image: new_phone,
         name: "New Phones",
         path: "new_phones",
       },
-      { id: "prexo", image: prexo, name: "PREXO", path: "prexo" },
-      { id: "openBox", image: openBox, name: "OPEN-BOX", path: "open_box" },
+      { id: "prexo", image: prexo, name: "Prexo", path: "prexo" },
+      { id: "openBox", image: openBox, name: "Open Box", path: "open_box" },
     ],
   },
   {
@@ -30,26 +30,26 @@ export const dropdowns = [
     title: "Manage Masters",
     options: [
       { id: "vrp", image: vrp, name: "VRP", path: "masters/vrp" },
-      { id: "spares", image: spare, name: "SPARES", path: "masters/spares" },
+      { id: "spares", image: spare, name: "Spares", path: "masters/spares" },
       {
         id: "new_phones",
         image: new_phone,
         name: "New Phones",
         path: "masters/new_phones",
       },
-      { id: "prexo", image: prexo, name: "PREXO", path: "masters/prexo" },
-      { id: "openBox", image: openBox, name: "OPEN-BOX", path: "masters/open_box" },
+      { id: "prexo", image: prexo, name: "Prexo", path: "masters/prexo" },
+      { id: "openBox", image: openBox, name: "Open Box", path: "masters/open_box" },
     ],
   },
  
 ];
 
 export const withoutDropdowns = [
-  { id: "home", image: dashboard, name: "HOME", path: "/" },
+  { id: "home", image: dashboard, name: "Home", path: "/" },
   {
     id: "advertisement",
     image: advertisement,
-    name: "ADVERTISEMENT",
+    name: "Advertisement",
     path: "advertisement",
   },
   {
